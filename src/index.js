23e525bc41d68803b15abe3e5513b378d71665a9
pdiomede <pdiomede@yahoo.com>
2022-06-27 14:32:22 +0200

    New changes applied

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function getButtonText() {
+    return 'Clcik on me!';
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Create a React component
+/*
+const App = function() {
+    return <div>Hi there!</div>;
+};
+*/
+
+// Is it possible to use also this other syntax
+const App = () => {
+
+    return (
+        <div className="UI container comments">
+            
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" />
+                </a>
+            </div>
+
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+            </div>
+
+            <div className="metadata">
+                <span className="date">Today at 6:00PM</span>
+            </div>
+
+            <div className="text">Nice Blog Post</div>
+
+
+        </div>
+    );
+
+
+    /*
+    const buttonText = {text: 'Click Me!'};
+    const labelText = 'Enter name:';
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
+    */
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(<App />,document.querySelector('#root'));
